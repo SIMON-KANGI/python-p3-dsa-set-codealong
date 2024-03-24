ba88e74fa07a0d5b0e301f440aed23948e3a0fc2
SIMON-KANGI <simonmwangikangi@gmail.com>
2024-03-24 09:26:13 +0300

    completed

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,19 @@
 class MySet:
+    def __init__(self, enumarable=[]):
+        self.dictionary={}
+        for value in enumarable:
+            self.dictionary[value] = True
 
-    pass
+    def has(self,value):
+        return value in self.dictionary
+    
+    def add(self,value):
+        self.dictionary[value] = True
+        return self
+    
+    def delete(self,value):
+        self.dictionary.pop(value,None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
